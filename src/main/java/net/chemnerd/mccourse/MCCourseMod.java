81a242f62d835d2dd1f8b7004d820ce4d7f85f9f
chemistrynerd99 <pileofpixels2022@gmail.com>
2024-02-07 19:21:33 -0400

    Adding Alexandrite - Ingredients tab, 2D texture with item/generated texture type

--- src/main/java/net/chemnerd/mccourse/MCCourseMod.java
+++ src/main/java/net/chemnerd/mccourse/MCCourseMod.java
@@ -1,6 +1,8 @@
 package net.chemnerd.mccourse;
 
 import com.mojang.logging.LogUtils;
+import net.chemnerd.mccourse.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -22,6 +24,9 @@ public class MCCourseMod {
     public MCCourseMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+
+        ModItems.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
@@ -32,9 +37,11 @@ public class MCCourseMod {
 
     }
 
-    // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event) {
 
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.ALEXANDRITE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
